c3a1fdfde95ebb635f7fb010d22f5f3ba862dbb8
Denis Ilyasov <LazyZombieNya@gmail.com>
2023-08-19 22:43:06 +0500

    add MVVM

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -3,8 +3,10 @@ package ru.netology.nmedia
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import androidx.activity.viewModels
 import ru.netology.nmedia.databinding.ActivityMainBinding
 import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.viewmodel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,48 +14,25 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val post = Post(
-            id = 1,
-            author = "Нетология. Университет интернет-профессий будущего",
-            content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
-            published = "1 мая в 18:36",
-            likedByMe = false,
-            likes = 999,
-            shares = 999999,
-            views = 9999999
-        )
-        with(binding) {
-            author.text = post.author
-            published.text = post.published
-            content.text = post.content
-            countLike.text = shortNumber(post.likes)
-            countShare.text = shortNumber(post.shares)
-            countView.text = shortNumber(post.views)
-            if (post.likedByMe) {
-                like?.setImageResource(R.drawable.ic_baseline_favorite_24)
-            }
-            like?.setOnClickListener {
-                println("clicked like")
-                post.likedByMe = !post.likedByMe
-                if (post.likedByMe) post.likes++ else post.likes--
-
-                like.setImageResource(
-                    if (post.likedByMe) R.drawable.ic_baseline_favorite_24
-                    else R.drawable.ic_baseline_favorite_border_24
-                )
+        val viewModel by viewModels<PostViewModel>()
+        viewModel.data.observe(this){post ->
+            with(binding) {
+                author.text = post.author
+                published.text = post.published
+                content.text = post.content
                 countLike.text = shortNumber(post.likes)
-            }
-            share?.setOnClickListener {
-                post.shares++
                 countShare.text = shortNumber(post.shares)
-            }
-            root?.setOnClickListener {
-                println("clicked root")
-            }
-            avatar?.setOnClickListener {
-                println("clicked avatar")
+                countView.text = shortNumber(post.views)
+                like?.setImageResource(if (post.likedByMe)R.drawable.ic_baseline_favorite_24 else R.drawable.ic_baseline_favorite_border_24)
             }
         }
+            binding.like?.setOnClickListener {
+                viewModel.like()
+            }
+            binding.share?.setOnClickListener {
+                viewModel.share()
+            }
+
     }
 
     private fun shortNumber(number: Int):String {
