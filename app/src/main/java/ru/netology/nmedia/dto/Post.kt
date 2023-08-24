115f0f17ee9f07745187f06601ec0610f440cd44
Denis Ilyasov <LazyZombieNya@gmail.com>
2023-08-25 01:44:58 +0500

    Add RecyclerView, ListAdapter

--- app/src/main/java/ru/netology/nmedia/dto/Post.kt
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -5,7 +5,7 @@ data class Post(
     val author: String,
     val published: String,
     val content: String,
-    var likedByMe: Boolean=false,
+    val likedByMe: Boolean=false,
     val likes: Int,
     val shares:Int,
     val views:Int
