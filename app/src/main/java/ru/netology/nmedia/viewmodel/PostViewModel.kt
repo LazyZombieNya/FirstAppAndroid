03047c9f07e2e9e0542b0056527d7d091552ebad
Denis Ilyasov <LazyZombieNya@gmail.com>
2023-09-05 01:19:26 +0500

    MaterialButton

--- app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -5,7 +5,8 @@ import androidx.lifecycle.ViewModel
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.repository.PostRepository
 import ru.netology.nmedia.repository.PostRepositoryInMemoryImpl
-private val empty= Post(
+
+private val empty = Post(
     id = 0,
     author = "",
     content = "",
@@ -15,24 +16,29 @@ private val empty= Post(
     shares = 0,
     views = 0
 )
-class PostViewModel: ViewModel() {
+
+class PostViewModel : ViewModel() {
     private val repository: PostRepository = PostRepositoryInMemoryImpl()
     val data = repository.getAll()
     val edited = MutableLiveData(empty)
 
 
-    fun changeContentAndSave(content:String){
+    fun changeContentAndSave(content: String) {
         edited.value?.let {
             val text = content.trim()
-            if (it.content!=text){
-                repository.save(it.copy(content= text))
+            if (it.content != text) {
+                repository.save(it.copy(content = text))
             }
         }
         edited.value = empty
     }
 
-    fun likeById(id:Long) = repository.likeById(id)
-    fun shareById(id:Long) = repository.shareById(id)
+    fun cancelEdit() {
+        edited.value = empty
+    }
+
+    fun likeById(id: Long) = repository.likeById(id)
+    fun shareById(id: Long) = repository.shareById(id)
     fun removeById(id: Long) = repository.removeById(id)
 
     fun edit(post: Post) {
