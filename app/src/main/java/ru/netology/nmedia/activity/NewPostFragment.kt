55b314526994300e641db9a9b0e540e4cc34cf0f
LazyZombieNya <42117322+LazyZombieNya@users.noreply.github.com>
2024-02-20 23:50:29 +0500

    edit Dagger Hilt and Singletons

--- app/src/main/java/ru/netology/nmedia/activity/NewPostFragment.kt
+++ app/src/main/java/ru/netology/nmedia/activity/NewPostFragment.kt
@@ -19,7 +19,7 @@ import androidx.core.view.MenuProvider
 import androidx.core.view.isGone
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.github.dhaval2404.imagepicker.ImagePicker
 import dagger.hilt.android.AndroidEntryPoint
@@ -42,8 +42,7 @@ class NewPostFragment : Fragment() {
     ): View {
 
         val binding = FragmentNewPostBinding.inflate(layoutInflater)
-        val viewModel: PostViewModel by viewModels(
-            ownerProducer = ::requireParentFragment)
+        val viewModel: PostViewModel by activityViewModels()
 
         val postContent = arguments?.text
 
